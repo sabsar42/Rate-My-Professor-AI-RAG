cdfac2a92e7880fce57aaf5fb8e015d18f4e62d4
Shakib Absar <sabsar42@gmail.com>
2024-08-29 03:10:01 +0600

    added

---
+++ app/api/chat/route.js
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const systemPrompt =
+`
+`
